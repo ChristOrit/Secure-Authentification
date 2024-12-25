3565d18eb4d7f17b9a6bd0b2a5ec45aa47ddea47
Lisan El Gaib <christ@christ-orit>
2024-12-25 13:49:58 +0100

    Ajout du code css de picocss

--- php-code/connexion.php
+++ php-code/connexion.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/picocss/dist/pico.min.css">
+    <title>Connexion1</title>
+    <link rel="stylesheet" href="pico.min.css">
 </head>
 <body>
     <div class="container">
--- php-code/inscription.php
+++ php-code/inscription.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/picocss/dist/pico.min.css">
+    <link rel="stylesheet" href="pico.min.css">
 </head>
 <body>
     <div class="container">
